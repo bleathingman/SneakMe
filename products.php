f638d34437cd0b43d3343229e1d38a2a80c03a34
Matheo <matheo.dourneau@gmail.com>
2023-04-17 21:53:49 +0200

    Style formulaire gestion de produits

--- products.php
+++ products.php
@@ -7,8 +7,6 @@ if (!isset($_SESSION["user_id"])) {
 	header("Location: login.php");
 	exit;
 }
-
-
 ?>
 
 <!DOCTYPE html>
@@ -27,65 +25,81 @@ if (!isset($_SESSION["user_id"])) {
 
 <body>
 	<?php require "templates/header.php"; ?>
+
+
 	<div class="container">
 		<h1>Gestion des produits</h1>
 
 		<!-- Formulaire de création de produit -->
 		<form class="form" action="gestion_produits/create_product.php" method="post" enctype="multipart/form-data">
 			<div class="form-group">
-				<label for="product_name">Nom du produit :</label>
-				<input type="text" id="product_name" name="product_name" required>
-				<label for="brand">Marque :</label>
-				<input type="text" id="brand" name="brand" required>
-				<label for="color">Couleur :</label>
-				<input type="text" id="color" name="color" required>
-				<label for="size">Taille :</label>
-				<input type="text" id="size" name="size" required>
-				<label for="price">Prix :</label>
-				<input type="number" id="price" name="price" step="0.01" min="0" required>
-				<label for="image">Image :</label>
-				<input type="file" id="image" name="image" accept="image/*">
-				<input class="btn btn-primary" type="submit" value="Ajouter un produit">
+				<div>
+					<label for="product_name">Nom du produit :</label>
+					<input type="text" id="product_name" name="product_name" required>
+				</div>
+				<div>
+					<label for="brand">Marque :</label>
+					<input type="text" id="brand" name="brand" required>
+				</div>
+				<div>
+					<label for="color">Couleur :</label>
+					<input type="text" id="color" name="color" required>
+				</div>
+				<div>
+					<label for="size">Taille :</label>
+					<input type="text" id="size" name="size" required>
+				</div>
+				<div>
+					<label for="price">Prix :</label>
+					<input type="number" id="price" name="price" step="0.01" min="0" required>
+				</div>
+				<div>
+					<label for="image">Image :</label>
+					<input type="file" id="image" name="image" accept="image/*">
+				</div>
 			</div>
+			<input class="btn btn-xp" type="submit" value="Ajouter un produit">
 		</form>
+		<br>
 
 		<!-- Tableau des produits -->
-		<table>
-			<thead>
-				<tr>
-					<!-- <th>Id</th> -->
-					<th>Image</th>
-					<th>Nom du produit</th>
-					<th>Marque</th>
-					<th>Couleur</th>
-					<th>Taille</th>
-					<th>Prix</th>
-					<th>Actions</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-				// récupération des données de la table products
-				$result = $conn->query("SELECT * FROM products");
-				while ($row = $result->fetch_assoc()) {
-					echo "<tr>";
-					// echo "<td>" . $row["id"] . "</td>";
-					echo "<td><img src='gestion_produits/uploads/" . $row["image"] . "' alt='" . $row["product_name"] . "' width='100'></td>";
-					echo "<td>" . $row["product_name"] . "</td>";
-					echo "<td>" . $row["brand"] . "</td>";
-					echo "<td>" . $row["color"] . "</td>";
-					echo "<td>" . $row["size"] . "</td>";
-					echo "<td>" . $row["price"] . "</td>";
-					echo "<td>";
-					echo "<a href='gestion_produits/edit_product.php?id=" . $row["id"] . "'>Modifier</a> | ";
-					echo "<a href='gestion_produits/delete_product.php?id=" . $row["id"] . "' onclick='return confirm(\"Êtes-vous sûr de vouloir supprimer ce produit ?\")'>Supprimer</a>";
-					echo "</td>";
-					echo "</tr>";
-				}
-				?>
-			</tbody>
-		</table>
-		</main>
+		<div class="scrollable">
+			<table>
+				<thead>
+					<tr>
+						<!-- <th>Id</th> -->
+						<th>Image</th>
+						<th>Nom du produit</th>
+						<th>Marque</th>
+						<th>Couleur</th>
+						<th>Taille</th>
+						<th>Prix</th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					// récupération des données de la table products
+					$result = $conn->query("SELECT * FROM products");
+					while ($row = $result->fetch_assoc()) {
+						echo "<tr>";
+						// echo "<td>" . $row["id"] . "</td>";
+						echo "<td><img src='gestion_produits/uploads/" . $row["image"] . "' alt='" . $row["product_name"] . "' width='100'></td>";
+						echo "<td>" . $row["product_name"] . "</td>";
+						echo "<td>" . $row["brand"] . "</td>";
+						echo "<td>" . $row["color"] . "</td>";
+						echo "<td>" . $row["size"] . "</td>";
+						echo "<td>" . $row["price"] . "</td>";
+						echo "<td>";
+						echo "<a class='btn btn-edit' href='gestion_produits/edit_product.php?id=" . $row["id"] . "'>Modifier</a>";
+						echo "<a class='btn btn-delete' href='gestion_produits/delete_product.php?id=" . $row["id"] . "' onclick='return confirm(\"Êtes-vous sûr de vouloir supprimer ce produit ?\")'>Supprimer</a>";
+						echo "</td>";
+						echo "</tr>";
+					}
+					?>
+				</tbody>
+			</table>
+		</div>
 	</div>
 </body>
 
