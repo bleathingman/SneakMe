90d90c8d1448b86a7a899b4b2edbfaa73455c3fd
Evan <allainevan1@gmail.com>
2023-04-18 20:35:18 +0200

    ajout credits

--- my-chatbot/src/views/AboutView.vue
+++ my-chatbot/src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>Ceci sera la future description du projet</h1>
+    <h1>Création d’une solution e-commerce sous forme de chatbot</h1>
   </div>
+    <h2>Projet réalisé par Mathéo, Evan et Yasmine</h2><br /><br />
+    <p>Nous avons réalisé ce projet dans le but de répondre à la demande de l'entreprise Sneak Me.</p>
 </template>
