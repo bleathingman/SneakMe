2e5a8b478eb0065b0794659995a778f6291d1757
YB0800 <beddiar.yasmine@gmail.com>
2023-05-21 21:39:52 +0200

    modification css

--- my-chatbot/src/App.vue
+++ my-chatbot/src/App.vue
@@ -16,7 +16,7 @@
 }
 
 nav {
-  padding: 30px;
+  padding: 20px;
 }
 
 nav a {
@@ -25,7 +25,7 @@ nav a {
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #8b6c42;
 }
 
 .sneaker-image {
